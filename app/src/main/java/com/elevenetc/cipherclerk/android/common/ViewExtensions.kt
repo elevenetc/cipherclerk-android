a26b14272543058473e612d720c22df6fc0573a5
elevenetc <shumvlesu@gmail.com>
2021-01-22 01:16:07 +0100

    Fix lost key keyboard events

--- app/src/main/java/com/elevenetc/cipherclerk/android/common/ViewExtensions.kt
+++ app/src/main/java/com/elevenetc/cipherclerk/android/common/ViewExtensions.kt
@@ -9,37 +9,42 @@ import android.view.inputmethod.InputMethodManager
  * https://developer.squareup.com/blog/showing-the-android-keyboard-reliably/
  */
 fun View.focusAndShowKeyboard() {
-    /**
-     * This is to be called when the window already has focus.
-     */
-    fun View.showTheKeyboardNow() {
-        if (isFocused) {
-            post {
-                // We still post the call, just in case we are being notified of the windows focus
-                // but InputMethodManager didn't get properly setup yet.
-                val imm =
-                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-            }
+
+    post {
+        requestFocus()
+
+        if (hasWindowFocus()) {
+            // No need to wait for the window to get focus.
+            showTheKeyboardNow()
+        } else {
+
+            // We need to wait until the window gets focus.
+            viewTreeObserver.addOnWindowFocusChangeListener(
+                object : ViewTreeObserver.OnWindowFocusChangeListener {
+                    override fun onWindowFocusChanged(hasFocus: Boolean) {
+                        // This notification will arrive just before the InputMethodManager gets set up.
+                        if (hasFocus) {
+                            showTheKeyboardNow()
+                            // It’s very important to remove this listener once we are done.
+                            viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                        }
+                    }
+                })
         }
     }
+}
 
-    requestFocus()
-    if (hasWindowFocus()) {
-        // No need to wait for the window to get focus.
-        showTheKeyboardNow()
-    } else {
-        // We need to wait until the window gets focus.
-        viewTreeObserver.addOnWindowFocusChangeListener(
-            object : ViewTreeObserver.OnWindowFocusChangeListener {
-                override fun onWindowFocusChanged(hasFocus: Boolean) {
-                    // This notification will arrive just before the InputMethodManager gets set up.
-                    if (hasFocus) {
-                        this@focusAndShowKeyboard.showTheKeyboardNow()
-                        // It’s very important to remove this listener once we are done.
-                        viewTreeObserver.removeOnWindowFocusChangeListener(this)
-                    }
-                }
-            })
+/**
+ * This is to be called when the window already has focus.
+ */
+fun View.showTheKeyboardNow() {
+    if (isFocused) {
+        post {
+            // We still post the call, just in case we are being notified of the windows focus
+            // but InputMethodManager didn't get properly setup yet.
+            val imm =
+                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
 }
